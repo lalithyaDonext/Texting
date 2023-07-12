b1d65e7d5b18591de46b825f796757ea6f924124
UdanaIsiwari <it20163372@my.sliit.lk>
2023-07-12 10:03:01 +0530

    seller post add screen

---
+++ src/Inputs/Live.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Live = () => {
+  return (
+    <div>
+    <select
+      className="absolute w-[67px] h-[37px]   rounded-[4px] pl-[10px]  bg-[#CFF3FB] placeholder:text-[rgba(30, 30, 30, 0.7)] font-normal font-inter text-[12px] leading-[15px] text-[#2B3EEB] outline-none"
+     
+    >
+    <option>Live</option>
+    </select>
+  </div>
+  )
+}
+
+export default Live
---
+++ src/Inputs/Presentage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './presentage.css';
+import { BiDownArrowAlt } from 'react-icons/bi';
+import { ReactComponent as ArrowDown } from '../images/icon/light/arrow/ArrowDown.svg'
+const Percentage = ({ placeholder, option }:any) => {
+  return (
+    <div className="percentage-drop-down-main">
+        <div className='presentage-placeholder'>
+            {placeholder}
+                <select className="percentage-drop-down-select" >
+                    <option  value="" disabled selected>%</option>
+                    <option>%</option>
+                </select>
+                <ArrowDown className="percentage-dropdown-arrow" />
+                </div>
+            </div>
+  );
+};
+
+export default Percentage;
