a117e5aec48fd46e52d5866e418ae7027ff43dc1
Manuka_Dahanayake <manukaud98@gmail.com>
2023-07-14 10:43:55 +0530

    Create popoup message box

--- src/UserBackground/User/Userbackground.tsx
+++ src/UserBackground/User/Userbackground.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './UserBackground.css';
 import { PiCaretUpDownFill } from 'react-icons/pi';
 import SideBar from '../SideBar/SideBar';
-// import Header from '../Header/Header';
+import Header from '../Header/Header';
 import Posts from '../../Seller/SellerPostViewProcess/Posts';
 import PostStepTwo from '../../Seller/SellerPostViewProcess/postStepTwo';
 import SellerFolowers from '../../Seller/SellerFollowers/SellerFolowers';
@@ -22,6 +22,8 @@ import EditPostStatus from '../../Seller/SellerPostEdit/EditPostStatus';
 import AddPostTwo from '../../Seller/SellerPostAdd/AddPostTwo';
 import AddpostSuccess from '../../Seller/SellerPostAdd/AddPostSuccess';
 import BuyerProfile from '../../Buyer/BuyerProfile/BuyerProfile';
+import SmsSentSuccess from '../../PopUpModals/SmsSentSuccess';
+import SendSms from '../../PopUpModals/SendSms';
 
 
 const UserBackground = () => {
@@ -31,7 +33,7 @@ const UserBackground = () => {
 
   return (
     <div className="page-container">
-      {/* <Header userType={userType} username={username} /> */}
+      <Header userType={userType} username={username} />
       <div className="content-container">
         <SideBar />
         <div className="page-content">
